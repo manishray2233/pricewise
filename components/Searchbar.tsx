7dd5bb0820067b018fd5a3b092a83585b9c34c2e
Manish Ray <manishray2233@gmail.com>
2023-11-13 22:35:57 +0530

    Working On SearchBar

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,9 +1,53 @@
-import React from 'react'
+"use client";
+
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.in") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+};
 
 const Searchbar = () => {
-  return (
-    <div>Searchbar</div>
-  )
-}
+  const [searchPrompt, setSearchPrompt] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Searchbar
+  const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
+
+    if (!isValidLink) return alert("Please provide a valid Amazon link");
+
+    try {
+    } catch (error) {}
+  };
+
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter Product Link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
+};
+
+export default Searchbar;
